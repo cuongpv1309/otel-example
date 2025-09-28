cdda5360572368c3e11ca2fba2b8305d87174dcd
phamvancuong.ktht <phamvancuong.ktht@vnpt.vn>
2025-09-28 18:19:17 +0700

    init branch

--- order-service/order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -1,10 +1,5 @@
 package com.example.order_service;
 
-import io.pyroscope.http.Format;
-import io.pyroscope.javaagent.EventType;
-import io.pyroscope.javaagent.PyroscopeAgent;
-import io.pyroscope.javaagent.config.Config;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -18,21 +13,4 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
-	@PostConstruct
-	public void init() {
-		System.out.println("Start initialize pyroscope serve");
-		PyroscopeAgent.start(
-				new Config.Builder()
-						.setApplicationName("order-app-java")
-						.setProfilingEvent(EventType.ITIMER)
-						.setFormat(Format.JFR)
-						.setServerAddress("http://pyroscope:4040")
-						// Optionally, if authentication is enabled, specify the API key.
-						// .setBasicAuthUser("<User>")
-						// .setBasicAuthPassword("<Password>")
-						// Optionally, if you'd like to set allocation threshold to register events, in bytes. '0' registers all events
-						// .setProfilingAlloc("0")
-						.build()
-		);
-	}
 }
